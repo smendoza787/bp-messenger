eed0ff0951feaf847029e2c0c582a9245be14250
Sergio Mendoza <smendoza787@gmail.com>
2017-10-23 17:53:41 -0700

    generate subscription model with associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024004807) do
+ActiveRecord::Schema.define(version: 20171024005211) do
 
   create_table "chats", force: :cascade do |t|
     t.string   "identifier"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20171024004807) do
     t.integer  "chat_id"
     t.index ["chat_id"], name: "index_messages_on_chat_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "chat_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_subscriptions_on_chat_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
