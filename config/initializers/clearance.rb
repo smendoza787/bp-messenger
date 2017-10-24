1b542fa89fc194b10da4e17416e17d90c5acfb90
Sergio Mendoza <smendoza787@gmail.com>
2017-10-24 03:43:32 -0700

    install friendly_id gem and generate clearance routes

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,5 @@
 Clearance.configure do |config|
+  config.routes = false
   config.mailer_sender = "smendoza787@gmail.com"
   config.rotate_csrf_on_sign_in = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  mount ActionCable.server => '/cable'
   root 'chats#index'
-  resources :users, only: [:show] do
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
     resources :chats, only: [:index, :create, :show]
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
   end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+  mount ActionCable.server => '/cable'
 end
