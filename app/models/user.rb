d578a83e83be691f26927a26f36d98556d455da0
Sergio Mendoza <smendoza787@gmail.com>
2017-10-23 21:20:36 -0700

    add existing_chats_users method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  has_many :chats, dependent: :destroy
   has_many :messages, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
+  has_many :chats, through: :subscriptions
 
   def name
     email.split('@')[0]
+  end
+
+  def existing_chats_users
+    existing_chat_users = []
+    self.chats.each do |chat|
+      existing_chat_users.concat(chat.subscriptions.where.not(user_id: self.id).map{ |sub| sub.user })
+    end
+    existing_chat_users.uniq
   end
 end
