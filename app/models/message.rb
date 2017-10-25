5b0ea20145cfdf1512c81a64b8cb6825d6ec22cc
Sergio Mendoza <smendoza787@gmail.com>
2017-10-24 22:58:07 -0700

    style messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,10 +2,14 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat
   validates :body, presence: true
-  after_create_commit :broadcast_message
+  after_create_commit :broadcast_message, :get_time
 
   private
-    def broadcast_message
-      MessageBroadcastJob.perform_later(self)
-    end
+  def broadcast_message
+    MessageBroadcastJob.perform_later(self)
+  end
+
+  def get_time
+    @time_created = Time.now
+  end
 end
