9eda3dafacab62d6dd56e9a66be35c90a13decf9
Sergio Mendoza <smendoza787@gmail.com>
2017-12-02 00:14:40 -0800

    delete add timestamp migration, add timestamp to message migration instead

--- db/migrate/20171024005821_create_messages.rb
+++ db/migrate/20171024005821_create_messages.rb
@@ -1,6 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
+      t.timestamps null: false
       t.string :body
       t.belongs_to :user, foreign_key: true
       t.belongs_to :chat, foreign_key: true
--- db/migrate/20171201213501_add_timestamp_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddTimestampToMessages < ActiveRecord::Migration[5.0]
-  def change
-    add_column :messages, :created_at, :timestamps
-  end
-end
